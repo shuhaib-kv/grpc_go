93a88f5ddcca9981434709392471cd033dbe9842
shuhaib <kvshuhaib2@gmail.com>
2022-12-28 16:55:17 +0530

    updated greet request

--- greet/greet_server/server.go
+++ greet/greet_server/server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bo/greet/greetpb"
+	"context"
 	"fmt"
 	"log"
 	"net"
@@ -11,6 +12,15 @@ import (
 
 type server struct{}
 
+func (*server) Greet(ctx context.Context, req *greetpb.GreetRequest) (*greetpb.GreetResponse, error) {
+	firstname := req.GetGreeting().GetFirstName()
+	result := "hello" + firstname
+	res := &greetpb.GreetResponse{
+		Result: result,
+	}
+	return res, nil
+}
+
 func main() {
 	fmt.Println("hello")
 	lis, err := net.Listen("tcp", "0.0.0.0:50051")
@@ -19,7 +29,7 @@ func main() {
 
 	}
 	s := grpc.NewServer()
-	greetpb.RegisterGreetServiceServer(s, server{})
+	greetpb.RegisterGreetServiceServer(s, &server{})
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("faild:%v", err)
 	}
